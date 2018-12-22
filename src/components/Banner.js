c4f044247e3a576167bcce6fbd64b9de6bcde155
Federico Sannicolò <mirluin@gmail.com>
2018-12-22 17:30:43 +0100

    start

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>RINGTIRE</h1>
+            </header>
+            <div className="content">
+                <p>A valuable step,<br />
+                pushing your standards forward.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Learn more</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
